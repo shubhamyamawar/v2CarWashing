92f4529f8eabad0d777436c5c79a4aeeb0a2b2f1
shubhamyamawar <65944115+shubhamyamawar@users.noreply.github.com>
2023-10-02 17:14:01 +0530

    Delete CarWashWasher directory

--- CarWashWasher/src/main/java/com/washer/exception/WasherNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.washer.exception;
-
-public class WasherNotFoundException extends RuntimeException{
-
-	public WasherNotFoundException(String msg) {
-		// TODO Auto-generated constructor stub
-		super(msg);
-	}
-}
